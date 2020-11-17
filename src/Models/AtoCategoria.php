3966276ffea4cd93a06ff74327e567840df60630
Diego Neumann <diego-neumann@hotmail.com>
2020-11-17 01:39:07 +0000

    Ajustando projeto

---
+++ src/Models/AtoCategoria.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Oka6\SulRadio\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class AtoCategoria extends Model
+{
+    const TABLE = 'ato_categoria';
+    protected $fillable = [
+
+    ];
+    protected $connection = 'sulradio';
+    protected $table = 'ato_categoria';
+		
+
+    
+
+}
