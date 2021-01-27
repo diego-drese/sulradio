32624ed59e0b87eed6a516c86c01f9a78b6951eb
Diego Neumann <diego-neumann@hotmail.com>
2021-01-27 15:10:16 +0000

    Ajustado mascara de cnpj

--- src/Resources/views/backend/emissora/form.blade.php
+++ src/Resources/views/backend/emissora/form.blade.php
@@ -292,6 +292,7 @@
     </style>
 @endsection
 @section('script_footer_end')
+    <script type="text/javascript" src={{mix('/vendor/oka6/admin/js/forms.js')}}></script>
     <script type="text/javascript" src={{mix('/vendor/oka6/admin/js/datatables.js')}}></script>
     <script type="text/javascript" src={{mix('/vendor/oka6/admin/js/select2.js')}}></script>
     <script>
