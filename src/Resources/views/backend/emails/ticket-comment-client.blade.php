0bb9282d05790c75de323cfe47d54b6b2661e3dc
Diego <diego-neumann@hotmail.com>
2026-01-20 17:25:41 -0300

    Ajustado texto de emails

--- src/Resources/views/backend/emails/ticket-comment-client.blade.php
+++ src/Resources/views/backend/emails/ticket-comment-client.blade.php
@@ -1,17 +1,16 @@
 @component('mail::message')
 <i><b style="color: #d21325">*</b> Esta é uma mensagem automática. Por favor, não responda!</i>
 <hr />
-<h1>Prezado cliente SEAD - Sulradio.</h1>
+<h1>SULRADIO – Atualização de Processo</h1>
 <div>
-Acesse o sistema SEAD - Sulradio para ler e interagir com um processo de seu interesse através do botão abaixo.
+Uma nova atualização está disponível no sistema SEAD.
 </div>
 <hr />
-Para interagir com essa solicitação acesse:
+Acessar o processo:
 @component('mail::button', ['url' => route('ticket.client.answer', [$data->identify])])
 ACESSE AQUI
 @endcomponent
 <i><b style="color: #d21325">*</b> Esta é uma mensagem automática. Por favor, não responda!</i>
 Obrigado,
-
 {{ config('app.name') }}
 @endcomponent
